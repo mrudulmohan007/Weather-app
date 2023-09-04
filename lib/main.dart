d56dab64c9663436c4610e0cd1af97267938ca44
Mrudul Mohan <mrudulmohan2001@gmail.com>
2023-09-04 13:57:37 +0530

    ui outline done

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
